c8a6c14e25b1f44573ac00bf19b32b23e31f28b8
Izzath Isharah <79443583+IzzathIsharah@users.noreply.github.com>
2022-04-22 11:48:31 +0530

    init

---
+++ resources/views/employees-mgmt/create.blade.php
@@ -0,0 +1,197 @@
+
+
+@extends('employees-mgmt.base')
+
+@section('action-content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Add New Employee</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('employee-management.store') }}" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+                            <label for="firstname" class="col-md-4 control-label">Employee Name</label>
+
+                            <div class="col-md-6">
+                                <input id="firstname" type="text" class="form-control" name="firstname" value="{{ old('firstname') }}" required autofocus>
+
+                                @if ($errors->has('firstname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('firstname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+                            <label for="lastname" class="col-md-4 control-label">Last Name</label>
+
+                            <div class="col-md-6">
+                                <input id="lastname" type="text" class="form-control" name="lastname" value="{{ old('lastname') }}" required>
+
+                                @if ($errors->has('lastname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('lastname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('middlename') ? ' has-error' : '' }}">
+                            <label for="middlename" class="col-md-4 control-label">Middle Name</label>
+
+                            <div class="col-md-6">
+                                <input id="middlename" type="text" class="form-control" name="middlename" value="{{ old('middlename') }}" required>
+
+                                @if ($errors->has('middlename'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('middlename') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label for="address" class="col-md-4 control-label">Salary</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}" required>
+
+                                @if ($errors->has('address'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Country</label>
+                            <div class="col-md-6">
+                                <select class="form-control js-country" name="country_id">
+                                    <option value="-1">Please select your country</option>
+                                    @foreach ($countries as $country)
+                                        <option value="{{$country->id}}">{{$country->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">State</label>
+                            <div class="col-md-6">
+                                <select class="form-control js-states" name="state_id">
+                                    <option value="-1">Please select your state</option>
+                                  @foreach ($states as $state)
+                                        <option value="{{$state->id}}">{{$state->name}}</option>
+                                    @endforeach 
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">City</label>
+                            <div class="col-md-6">
+                                <select class="form-control js-cities" name="city_id">
+                                    <option value="-1">Please select your city</option>
+                                  @foreach ($cities as $city)
+                                        <option value="{{$city->id}}">{{$city->name}}</option>
+                                    @endforeach  
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('zip') ? ' has-error' : '' }}">
+                            <label for="zip" class="col-md-4 control-label">Zip</label>
+
+                            <div class="col-md-6">
+                                <input id="zip" type="text" class="form-control" name="zip" value="{{ old('zip') }}" required>
+
+                                @if ($errors->has('zip'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('zip') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('age') ? ' has-error' : '' }}">
+                            <label for="zip" class="col-md-4 control-label">Mobile Number</label>
+                            
+
+                            <div class="col-md-6">
+                                <input id="age" type="text" class="form-control" pattern="[0]{1}[0-9]{9}" name="age" value="{{ old('age') }}" required>
+
+                                @if ($errors->has('age'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('age') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Experiance</label>
+                            <div class="col-md-6">
+                                <div class="input-group date">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-anchor"></i>
+                                    </div>
+                                    <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}" required>
+                                </div>
+                            </div>
+                        </div>
+                          <div class="form-group">
+                            <label class="col-md-4 control-label">Education</label>
+                            <div class="col-md-6">
+                                <div class="input-group date">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-graduation-cap"></i>
+                                    </div>
+                                    <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('department_id') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Department</label>
+                            <div class="col-md-6">
+                                <select class="form-control" name="department_id">
+                                    @foreach ($departments as $department)
+                                        <option value="{{$department->id}}">{{$department->name}}</option>
+                                    @endforeach
+                                </select>
+                                 @if ($errors->has('department_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('department_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('division_id') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Division</label>
+                            <div class="col-md-6">
+                                <select class="form-control" name="division_id">
+                                    @foreach ($divisions as $division)
+                                        <option value="{{$division->id}}">{{$division->name}}</option>
+                                    @endforeach
+                                </select>
+                                 @if ($errors->has('division_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('division_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                           
+                            <div class="col-md-6">
+                              
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Create
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection 
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      HMS
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="#">HOSPITAL MANAGEMENT SYSTEM</a>.</strong> All rights reserved.
+  </footer>
